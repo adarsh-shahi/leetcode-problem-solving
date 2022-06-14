bbaa6d8c017e537e33ab8a246cd1a3039561eb73
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-14 12:27:20 +0530

    add comment

--- 1389.java
+++ 1389.java
@@ -1,5 +1,6 @@
 class solve {
     // https://leetcode.com/problems/create-target-array-in-the-given-order/
+    // 0 <= index[i] <= i      -- thats why its easy
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 3, 4};
         int[] index = {0,1,4,2,1};
