d2d7efe1a3bb9a3e8102dd184a6847927f7ab0ac
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-15 10:24:03 +0530

    init

---
+++ 2006.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class solve {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,1};
+        System.out.println(solve(nums, 1));
+    }
+
+    private static int solve(int[] nums, int k){
+        int count = 0;
+        for(int i = 0; i < nums.length; ++i){
+            for(int j = i + 1; j < nums.length; ++j){
+                if(Math.abs((nums[i] - nums[j])) == k) 
+                    count++;
+            }
+        }
+        return count;
+    }
+}
