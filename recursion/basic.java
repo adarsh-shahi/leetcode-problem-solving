2da262379c1d2a09e923a18dcbdf6ad7c70444c8
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-20 16:27:00 +0530

    init

--- recursion/basic.java
+++ recursion/basic.java
@@ -4,11 +4,12 @@ public class basic {
 
     public static void main(String[] args) {
 
-        System.out.println(solve("adarsh"));
+        // System.out.println(solve("adarsh shahi"));
+        System.out.println(solve2("app a day keeps applefg away"));
         
     }
 
-    static String solve(String word){
+    static String solve(String word){  // skip a char
 
         String ans = "";
 
@@ -18,6 +19,16 @@ public class basic {
 
         return ans += solve(word.substring(1));
 
+    }
+
+    static String solve2(String sentence){  // skips a string
+
+        if(sentence.length() == 0) return "";
+
+        if(sentence.startsWith("app") && !sentence.startsWith("apple")) 
+            return solve2(sentence.substring(3));
+        else 
+            return sentence.charAt(0) + solve2(sentence.substring(1));
 
     }
     
