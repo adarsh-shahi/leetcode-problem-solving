ff5c8493bfe1c2f416472fe8bce1e840d3eb94fc
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-22 12:58:54 +0530

    reset i bit

--- bitwise/findiBit.java
+++ bitwise/findiBit.java
@@ -3,7 +3,7 @@ package bitwise;
 public class findiBit {
 
     public static void main(String[] args) {
-        System.out.println(set(11, 3));
+        System.out.println(reset(11, 2));
     }
 
     static int find(int num, int i){
@@ -18,5 +18,11 @@ public class findiBit {
         // int mask =(int) Math.pow(2, i-1);
         int mask = 1 << i-1;
         return num | mask;
+    }
+
+    static int reset(int num, int i){
+        int mask = 1 << i - 1;
+        mask = ~mask;
+        return num & mask;
     }
 } 
