8150fb784e3c5c42733269075c246d4a717c5775
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-04 19:42:41 +0530

    1ms static array

--- 682.java
+++ 682.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class solve {
     public static void main(String[] args) {
-        System.out.println(solve(new String[] {"1","C"}));
+        System.out.println(solve2(new String[] {"1", "C"}));
     }
 
     private static int solve(String[] ops){
@@ -27,5 +27,36 @@ class solve {
       for(Integer e: scores) sum+=e;
       return sum;
     }
+
+    private static int solve2(String[] ops){
+     int[] scores = new int[1000];
+     int s = 0;
+
+      for (int i = 0; i < ops.length; i++) {
+        if(ops[i] == "+"){
+          scores[s] = scores[s - 1] + scores[s - 2];
+          s++;
+        }
+        else if(ops[i] == "D"){
+          scores[s] = scores[s - 1] * 2;
+          s++;
+        }
+        else if(ops[i] == "C"){
+          s--;
+        }
+        else{
+          scores[s] = Integer.parseInt(ops[i]);
+          s++;
+        }
+      }
+      int sum = 0;
+
+      for (int i = 0; i < s; i++) {
+        sum += scores[i];
+      }
+      return sum;
+    }
+
+    
     
 }
