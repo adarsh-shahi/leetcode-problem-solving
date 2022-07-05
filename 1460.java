7100f16946abb264b66d85cef3260d4075abad65
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-05 23:08:44 +0530

    O(n)

--- 1460.java
+++ 1460.java
@@ -2,31 +2,29 @@
   import java.util.*;
 class solve {
     public static void main(String[] args) {
-        System.out.println(solve(new int[] {1,2,2,3}, new int[] {1,1,2,3}));
+        System.out.println(solve(new int[] {3,7,9}, new int[] {3,7,11}));
     }
 
     private static boolean solve(int[] target, int[] arr){
           
-      Arrays.sort(target);
-      Arrays.sort(arr);
-      for(int  i = 0 ;i < arr.length; ++i){
-        if(target[i] != arr[i]) return false;
-      }
-      // for (int i = 0; i < arr.length; i++) {
-      //   boolean flag = false;
-      //   for (int j = 0; j < target.length; j++) {
-      //     if(arr[i] == target[j]) flag = true;
-      //   }
-      //   if(!flag) return false;
-      // }
+     Map<Integer, Integer> targetMap = new HashMap<>();
+     Map<Integer, Integer> arrMap = new HashMap<>();
+     
+     for (int i = 0; i < target.length; i++) {
+      if(targetMap.containsKey(target[i])) targetMap.put(target[i], targetMap.get(target[i]) + 1);
+      else targetMap.put(target[i], 1);
+      if(arrMap.containsKey(arr[i])) arrMap.put(arr[i], arrMap.get(arr[i]) + 1);
+      else arrMap.put(arr[i], 1);
+     }
 
-      // for (int i = 0; i < target.length; i++) {
-      //   boolean flag = false;
-      //   for (int j = 0; j < arr.length; j++) {
-      //     if(arr[j] == target[i]) flag = true;
-      //   }
-      //   if(!flag) return false;
-      // }
+    
+
+     System.out.println(targetMap + " " + arrMap);
+
+     for(int key: targetMap.keySet()){
+      if(targetMap.get(key) == arrMap.get(key)) continue;
+      else return false;
+     }
 
      return true;
     }
