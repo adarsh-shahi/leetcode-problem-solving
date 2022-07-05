8dcdd6e8c8903edad7d2da6f56c6b1204f136408
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-05 20:53:14 +0530

    O(n)

--- 1217.java
+++ 1217.java
@@ -1,12 +1,24 @@
 import java.util.*;
 class solve {
     public static void main(String[] args) {
-        System.out.println(solve(new int[] {2, 3,3}));
+        System.out.println(solve(new int[] {11}));
     }
 
     private static int solve(int[] position){
+      boolean even = false;
+      boolean odd = false;
+      int firstOdd = Integer.MIN_VALUE;
+      int firstEven = Integer.MIN_VALUE;
       HashMap<Integer, Integer> hashMap = new HashMap<>();
       for (int i = 0; i < position.length; i++) {
+        if((position[i] & 1) == 1){ 
+          odd = true;
+          firstOdd = position[i];
+        }
+        else {
+          even = true;
+          firstEven = position[i];
+        }
         int count = 0;
         for (int j = 0; j < position.length; j++) {
           if(position[i] == position[j]) count++;
@@ -14,31 +26,32 @@ class solve {
         hashMap.put(position[i], count);
       }
 
-      int cost = Integer.MAX_VALUE;
+      if(even && !odd) return cost(hashMap, firstEven, false);
+      else if(odd && !even) return cost(hashMap, firstOdd, true);
+      else{
+        int cost1 = cost(hashMap, firstEven, false);
+        int cost2 = cost(hashMap, firstOdd, true);
+        if(cost1 < cost2) return cost1;
+        return cost2;
+      }
+      
+    }
 
-      for (int currentKey: hashMap.keySet()) {
-        int currentCost = 0;
-        int minKey = currentKey;
-  
-        boolean isOdd = (minKey & 1) == 1;
-        
+    static int cost(HashMap<Integer, Integer> hashMap, int minKey,boolean isOdd){
+      int cost = 0;
+
         for(int key: hashMap.keySet()){
           if(minKey == key) continue;
   
          System.out.println(key + " " + hashMap.get(key));
           if(isOdd){
-            if((key & 1) != 1) currentCost += hashMap.get(key);
+            if((key & 1) != 1) cost += hashMap.get(key);
           }
           else{
-            if((key & 1) == 1) currentCost += hashMap.get(key);
-          }
+            if((key & 1) == 1) cost += hashMap.get(key);
         }
-        System.out.println("current cost" + currentCost);
-
-        if(cost > currentCost) cost = currentCost;
-        
-      }
-      return cost;
     }
-    
+
+    return cost;
+  }
 }
