5c0f14a9ae7342f7d5ec34fe1f7a9d130e153b24
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-07 21:07:40 +0530

    O(n)

--- 977.java
+++ 977.java
@@ -1,14 +1,23 @@
 import java.util.*;
 class solve {
     public static void main(String[] args) {
-        for(int e: solve(new int[] {-4,-1,0,3,10})) System.out.println(e);
+        for(int e: solve(new int[] {-4,-1,0,0,3,10})) System.out.println(e);
     }
 
     private static int[] solve(int[] nums){
-     for (int i = 0; i < nums.length; i++) 
-      nums[i] = (int) Math.pow(nums[i], 2);
-     Arrays.sort(nums);
-     return nums;
+      int i = 0;
+      int j = nums.length - 1;
+      int p = nums.length - 1;
+      int[] ans = new int[nums.length];
+      while(p > -1){
+        if(Math.abs(nums[i]) > Math.abs(nums[j]))
+          ans[p] = nums[i] * nums[i++];
+        else 
+          ans[p] = nums[j] * nums[j--];
+        p--;    
+      }
+      return ans;
+     
     }
     
 }
