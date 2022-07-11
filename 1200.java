a0877dc893208732f958b72e6effc79ef5ee87d7
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-11 22:36:54 +0530

    bad time complexity but it works

---
+++ 1200.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class solve {
+  public static void main(String[] args) {
+    System.out.println(solve(new int[] {3,8,-10,23,19,-4,-14,27}));
+  }
+
+  private static List<List<Integer>> solve(int[] arr) {
+    List<List<Integer>> ans = new ArrayList<>();
+    int minDiff = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length - 1; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if(minDiff > Math.abs(arr[i] - arr[j])){
+          ans.clear();
+          minDiff = Math.abs(arr[i] - arr[j]);
+        }
+        if(minDiff == Math.abs(arr[i] - arr[j])){
+          List<Integer> point = new ArrayList<>();
+          if(arr[i] > arr[j]){
+            point.add(arr[j]);
+            point.add(arr[i]);
+          }
+          else {
+            point.add(arr[i]);
+            point.add(arr[j]);
+          }
+          ans.add(point);
+        }
+      }
+    }
+    for (int i = 0; i < ans.size(); i++) {
+      for (int j = 0; j < ans.size() - 1 - i; j++) {
+        if(ans.get(j).get(0) > ans.get(j + 1).get(0)){
+          List<Integer> temp = ans.get(j);
+          ans.set(j, ans.get(j + 1));
+          ans.set(j + 1, temp);
+        }
+      }
+    }
+    return ans;
+  }
+
+}
