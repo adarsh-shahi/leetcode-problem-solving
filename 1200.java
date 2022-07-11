db31e7a11f508f4d6cc621f36c9e600993c5910b
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-11 22:50:19 +0530

    nlogn :)

--- 1200.java
+++ 1200.java
@@ -2,44 +2,69 @@ import java.util.*;
 
 class solve {
   public static void main(String[] args) {
-    System.out.println(solve(new int[] {3,8,-10,23,19,-4,-14,27}));
+    System.out.println(solve(new int[] { 3, 8, -10, 23, 19, -4, -14, 27 }));
   }
 
-  private static List<List<Integer>> solve(int[] arr) {
+  static List<List<Integer>> solve(int[] arr) {
     List<List<Integer>> ans = new ArrayList<>();
     int minDiff = Integer.MAX_VALUE;
+    Arrays.sort(arr);
     for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = i + 1; j < arr.length; j++) {
-        if(minDiff > Math.abs(arr[i] - arr[j])){
-          ans.clear();
-          minDiff = Math.abs(arr[i] - arr[j]);
-        }
-        if(minDiff == Math.abs(arr[i] - arr[j])){
-          List<Integer> point = new ArrayList<>();
-          if(arr[i] > arr[j]){
-            point.add(arr[j]);
-            point.add(arr[i]);
-          }
-          else {
-            point.add(arr[i]);
-            point.add(arr[j]);
-          }
-          ans.add(point);
-        }
+      if (minDiff > Math.abs(arr[i] - arr[i + 1])) {
+        ans.clear();
+        minDiff = Math.abs(arr[i] - arr[i + 1]);
       }
-    }
-    for (int i = 0; i < ans.size(); i++) {
-      boolean isSorted = true;
-      for (int j = 0; j < ans.size() - 1 - i; j++) {
-        if(ans.get(j).get(0) > ans.get(j + 1).get(0)){
-          isSorted = false;
-          List<Integer> temp = ans.get(j);
-          ans.set(j, ans.get(j + 1));
-          ans.set(j + 1, temp);
+      if (minDiff == Math.abs(arr[i] - arr[i + 1])) {
+        List<Integer> point = new ArrayList<>();
+        if (arr[i] > arr[i + 1]) {
+          point.add(arr[i + 1]);
+          point.add(arr[i]);
+        } else {
+          point.add(arr[i]);
+          point.add(arr[i + 1]);
         }
+        ans.add(point);
       }
-      if(isSorted) return ans;
     }
     return ans;
   }
+  /*
+   * private static List<List<Integer>> solve(int[] arr) {
+   * List<List<Integer>> ans = new ArrayList<>();
+   * int minDiff = Integer.MAX_VALUE;
+   * for (int i = 0; i < arr.length - 1; i++) {
+   * for (int j = i + 1; j < arr.length; j++) {
+   * if(minDiff > Math.abs(arr[i] - arr[j])){
+   * ans.clear();
+   * minDiff = Math.abs(arr[i] - arr[j]);
+   * }
+   * if(minDiff == Math.abs(arr[i] - arr[j])){
+   * List<Integer> point = new ArrayList<>();
+   * if(arr[i] > arr[j]){
+   * point.add(arr[j]);
+   * point.add(arr[i]);
+   * }
+   * else {
+   * point.add(arr[i]);
+   * point.add(arr[j]);
+   * }
+   * ans.add(point);
+   * }
+   * }
+   * }
+   * for (int i = 0; i < ans.size(); i++) {
+   * boolean isSorted = true;
+   * for (int j = 0; j < ans.size() - 1 - i; j++) {
+   * if(ans.get(j).get(0) > ans.get(j + 1).get(0)){
+   * isSorted = false;
+   * List<Integer> temp = ans.get(j);
+   * ans.set(j, ans.get(j + 1));
+   * ans.set(j + 1, temp);
+   * }
+   * }
+   * if(isSorted) return ans;
+   * }
+   * return ans;
+   * }
+   */
 }
