8068b6f2787b7e896b1fccd269a1cbe6dd1dbfa6
adarsh-shahi <adarshshahi1009@gmail.com>
2022-07-26 20:18:20 +0530

    bf

---
+++ 2149.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class solve {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solve(new int[] {3,1,-2,-5,2,-4})));
+    }
+
+    private static int[] solve(int[] nums){
+      int[] nas = new int[nums.length];
+      int p = 0;
+      int l = nums.length - 1;
+      for(int i = 0, j = nums.length - 1; i < nums.length; i++, j-- ){
+        if(nums[i] > 0) {
+          nas[p] = nums[i];
+          p+=2;
+        }
+        if(nums[j] < 0 ) {
+          nas[l] = nums[j];
+          l-=2;
+        }
+        
+      }
+     return nas;
+    }
+    
+}
