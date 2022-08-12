d4acac4ac972903f3f2c58547ceb288d96670dab
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-12 20:00:09 +0530

    optimized still O(n^2)

--- 234.java
+++ 234.java
@@ -6,27 +6,27 @@ public class solve{
 
 public boolean isPalindrome(ListNode head) {
   if(head.next == null) return true;
-  int count = 0;
-  ListNode c = head;
-  while(c != null){
-      count++;
-      c = c.next;
-  }
-  int copy = count;
-  
-  for(ListNode i = head; i != null; i = i.next){
-      int first = i.val;
-      int lastP = copy - 1;
-      ListNode lastNode = head;
-      while(lastP != 0){
-          lastNode = lastNode.next;
-          lastP--;   
-      }
-      copy--;
-      if(first != lastNode.val) return false;
-      if(count == (copy / 2)) break;
-  }
-  return true;
+        int count = 0;
+        ListNode c = head;
+        while(c != null){
+            count++;
+            c = c.next;
+        }
+        int copy = count; //   COPY THIS LIST TO ARRAY :))))))))
+        int s = 1;  
+        for(ListNode i = head; i != null; i = i.next, s++){
+            int first = i.val;
+            int lastP = s;
+            ListNode lastNode = i;
+            while(lastP != copy){
+                lastNode = lastNode.next;
+                lastP++;   
+            }
+            copy--;
+            if(first != lastNode.val) return false;
+            if(count / 2 == copy) break;
+        }
+        return true;
 }
 }
 
