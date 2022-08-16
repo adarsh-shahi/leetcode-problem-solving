1124d05eb976c22b82dcb390a7b7e9b696df32ac
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-16 23:45:44 +0530

    return as list

--- recursion/permutations.java
+++ recursion/permutations.java
@@ -1,8 +1,24 @@
 import java.util.*;
 class solve {
     public static void main(String[] args) {
-        solve("","abc");
+        // solve("","abc");
+        System.out.println(solve2("", "car"));
     }
+
+    private static ArrayList<String> solve2(String p, String up){
+      if(up.isEmpty()){
+        ArrayList<String> list = new ArrayList<>();
+          list.add(p);
+          return list;
+      }
+      char f = up.charAt(0);
+      ArrayList<String> fl = new ArrayList<>();
+      for (int i = 0; i <= p.length(); i++) {
+        ArrayList<String> l = solve2(p.substring(0,i) + f + p.substring(i), up.substring(1));
+        fl.addAll(l);
+      }
+      return fl;
+  }
 
     private static void solve(String p, String up){
       if(up.isEmpty()){
