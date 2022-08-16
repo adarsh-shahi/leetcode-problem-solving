c3ed3e1682df803b81fc8e69c8b543d8998a6449
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-16 23:31:37 +0530

    bf

--- recursion/permutations.java
+++ recursion/permutations.java
@@ -6,14 +6,13 @@ class solve {
 
     private static void solve(String p, String up){
       if(up.isEmpty()){
-        System.out.println(p);
-        return;
+          System.out.println(p);
+          return;
       }
       char f = up.charAt(0);
       for (int i = 0; i <= p.length(); i++) {
-        solve(p.substring(0,i) + f + p.substring(i+1), up);
+          solve(p.substring(0,i) + f + p.substring(i), up.substring(1));
       }
-      solve(p, up.substring(1));
-    }
+  }
     
 }
