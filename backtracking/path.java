d09eaade98522064d3e922596d7fd802d467537e
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-18 06:53:24 +0530

    reducing redundancy

--- backtracking/path.java
+++ backtracking/path.java
@@ -1,19 +1,22 @@
 package backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class solve{
   public static void main(String[] args) {
      System.out.println(solve3("",0,0));
   }
+  /*
+        ArrayList<String> list = new ArrayList<>();
+        list.add(p);
+        return list;
+  */
   static ArrayList<String> solve3(String p, int r, int c){ // right , down and diagonal
     if(c >= 3 || r >= 3) return new ArrayList<>();
    
-    if(r == 2 && c == 2 ){
-      ArrayList<String> list = new ArrayList<>();
-      list.add(p);
-      return list;
-    }
+    if(r == 2 && c == 2 ) return new ArrayList<>(Arrays.asList(p));
+
     ArrayList<String> l1 = solve3(p+"R", r, c + 1);
     ArrayList<String> l2 = solve3(p+"D", r + 1, c);
     ArrayList<String> l3 = solve3(p+"X", r + 1, c + 1);
