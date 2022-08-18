9645beb623aa615545fb4694793eba2ee96fc46d
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-18 06:38:31 +0530

    return path as list

--- backtracking/path.java
+++ backtracking/path.java
@@ -1,8 +1,24 @@
 package backtracking;
 
+import java.util.ArrayList;
+
 class solve{
   public static void main(String[] args) {
-      solve("",0,0);
+     System.out.println(solve2("",0,0));
+  }
+
+  static ArrayList<String> solve2(String p, int r, int c){
+    if(c >= 3 || r >= 3) return new ArrayList<>();
+    // System.out.println(r + " " + c);
+    if(r == 2 && c == 2 ){
+      ArrayList<String> list = new ArrayList<>();
+      list.add(p);
+      return list;
+    }
+    ArrayList<String> l1 = solve2(p+"R", r, c + 1);
+    ArrayList<String> l2 = solve2(p+"D", r + 1, c);
+    l1.addAll(l2);
+    return l1;
   }
 
   static void solve(String p, int r, int c){
