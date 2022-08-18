4b791598958b6f43f5769ce2f1fe2f56e713a110
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-18 06:30:00 +0530

    print D and R for paths

--- backtracking/path.java
+++ backtracking/path.java
@@ -2,17 +2,17 @@ package backtracking;
 
 class solve{
   public static void main(String[] args) {
-      solve(0,0);
+      solve("",0,0);
   }
 
-  static void solve(int r, int c){
+  static void solve(String p, int r, int c){
     if(c >= 3 || r >= 3) return;
-    System.out.println(r + " " + c);
+    // System.out.println(r + " " + c);
     if(r == 2 && c == 2 ){
-      System.out.println("Found");
+      System.out.println(p);
       return;
     }
-    solve(r, c + 1);
-    solve(r + 1, c);
+    solve(p+"R", r, c + 1);
+    solve(p+"D", r + 1, c);
   }
-}
+} 
