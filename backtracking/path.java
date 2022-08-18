0e048a6e772bc375109df5fb8b2f59cb20be37d5
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-18 06:46:45 +0530

    return diagonal paths

--- backtracking/path.java
+++ backtracking/path.java
@@ -4,8 +4,23 @@ import java.util.ArrayList;
 
 class solve{
   public static void main(String[] args) {
-     System.out.println(solve2("",0,0));
+     System.out.println(solve3("",0,0));
   }
+  static ArrayList<String> solve3(String p, int r, int c){ // right , down and diagonal
+    if(c >= 3 || r >= 3) return new ArrayList<>();
+   
+    if(r == 2 && c == 2 ){
+      ArrayList<String> list = new ArrayList<>();
+      list.add(p);
+      return list;
+    }
+    ArrayList<String> l1 = solve3(p+"R", r, c + 1);
+    ArrayList<String> l2 = solve3(p+"D", r + 1, c);
+    ArrayList<String> l3 = solve3(p+"X", r + 1, c + 1);
+    l1.addAll(l2);
+    l1.addAll(l3);
+    return l1;
+  } 
 
   static ArrayList<String> solve2(String p, int r, int c){
     if(c >= 3 || r >= 3) return new ArrayList<>();
@@ -19,7 +34,7 @@ class solve{
     ArrayList<String> l2 = solve2(p+"D", r + 1, c);
     l1.addAll(l2);
     return l1;
-  }
+  } 
 
   static void solve(String p, int r, int c){
     if(c >= 3 || r >= 3) return;
