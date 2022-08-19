6ebff3dc9f2f5ad6c0152b64ac5b9f7f75cedd7d
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-19 09:25:03 +0530

    fibonacci

--- recursion/basic.java
+++ recursion/basic.java
@@ -5,7 +5,8 @@ public class basic {
     public static void main(String[] args) {
 
         // System.out.println(solve("adarsh shahi"));
-        System.out.println(solve2("app a day keeps applefg away"));
+        // System.out.println(solve2("app a day keeps applg away"));
+        System.out.println(fibonacci(6));
         
     }
 
@@ -18,18 +19,24 @@ public class basic {
         if(word.charAt(0) != 'a') ans += word.charAt(0); 
 
         return ans += solve(word.substring(1));
-
+                    
     }
 
     static String solve2(String sentence){  // skips a string
 
         if(sentence.length() == 0) return "";
 
-        if(sentence.startsWith("app") && !sentence.startsWith("apple")) 
+        if(sentence.startsWith("app") && !sentence.startsWith("apple")) // skips the words with app but not apple
             return solve2(sentence.substring(3));
         else 
             return sentence.charAt(0) + solve2(sentence.substring(1));
 
     }
-    
+
+    static int fibonacci(int n){
+        if(n == 1 || n == 2) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+     }
+
+     
 }
