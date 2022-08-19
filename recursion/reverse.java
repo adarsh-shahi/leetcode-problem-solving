bed7dc8a100837d1a147069574e9acd0edff06d1
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-19 09:07:40 +0530

    palindrome

--- recursion/reverse.java
+++ recursion/reverse.java
@@ -2,9 +2,14 @@ import java.util.Arrays;
 
 class soolve {
   public static void main(String[] args) {
-    int[] a = {1,2,3,4,5};
-    solve(a, 5);
-    System.out.println(Arrays.toString(a));
+    int[] a = {1,2,2,1};
+    // solve(a, 5);
+    System.out.println(solve1(a, 4));
+  }
+
+  static boolean solve1(int[] arr, int i){
+    if(i == (arr.length / 2)) return true;
+    return (arr[arr.length - i] == arr[i-1]) && solve1(arr, i - 1);
   }
 
   static void solve (int[] arr, int i){
