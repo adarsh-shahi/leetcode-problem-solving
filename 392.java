a394fef236bafa0d4fb535c8206ee656ca92f777
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-20 11:06:28 +0530

    return when find no need to go till end

--- 392.java
+++ 392.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class solve {
     public static void main(String[] args) {
-        System.out.println(isSubsequence("axc", "ahbgdc"));
+        System.out.println(isSubsequence("rjufvjafbxnbgriwgokdgqdqewn", "mjmqqjrmzkvhxlyruonekhhofpzzslupzojfuoztvzmmqvmlhgqxehojfowtrinbatjujaxekbcydldglkbxsqbbnrkhfdnpfbuaktupfftiljwpgglkjqunvithzlzpgikixqeuimmtbiskemplcvljqgvlzvnqxgedxqnznddkiujwhdefziydtquoudzxstpjjitmiimbjfgfjikkjycwgnpdxpeppsturjwkgnifinccvqzwlbmgpdaodzptyrjjkbqmgdrftfbwgimsmjpknuqtijrsnwvtytqqvookinzmkkkrkgwafohflvuedssukjgipgmypakhlckvizmqvycvbxhlljzejcaijqnfgobuhuiahtmxfzoplmmjfxtggwwxliplntkfuxjcnzcqsaagahbbneugiocexcfpszzomumfqpaiydssmihdoewahoswhlnpctjmkyufsvjlrflfiktndubnymenlmpyrhjxfdcq"));
     }
 
     static boolean isSubsequence(String s, String t) {
@@ -9,8 +9,15 @@ class solve {
     }
 
     private static boolean solve(String p, String up, String s){
-      if(up.isEmpty()) return s.equals(p) ? true : false;
-     return solve(p + up.charAt(0), up.substring(1), s) || solve(p, up.substring(1), s);
+      if(up.isEmpty()){
+        if(s.equals(p)) return true;
+        return false;
+      }
+      boolean flag = solve(p + up.charAt(0), up.substring(1), s);
+      if(flag) return flag;
+      boolean flag1 = solve(p, up.substring(1), s);
+      if(flag1) return flag1;
+      return false;
     }
     
 }
