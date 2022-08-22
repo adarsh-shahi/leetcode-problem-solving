fc2d47484ef2379b7ae70fb5d56e4301729130c0
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-22 15:51:06 +0530

    recursive

--- patterns/1.java
+++ patterns/1.java
@@ -1,6 +1,6 @@
 class solve{
   public static void main(String[] args) {
-    solve(5);
+    solve1(5,5);
   }
 
   static void solve(int n){
@@ -10,5 +10,14 @@ class solve{
       }
       System.out.println();
     }
+  }
+
+  static void solve1(int r, int c){
+    if(r == 0 )return;
+      for (int j = 0; j < c; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+      solve1(r - 1, c);
   }
 }
