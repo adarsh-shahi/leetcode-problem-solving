316eb581e486bbc409a3186755f028780dd9bc75
adarsh-shahi <adarshshahi1009@gmail.com>
2022-08-24 06:46:51 +0530

    floor of a target

--- binary-search/algorithm.java
+++ binary-search/algorithm.java
@@ -1,7 +1,7 @@
 class solve{
   public static void main(String[] args) {
     int[] arr = {2,3,5,9,14,16,18};
-    System.out.println(ceil(0, arr.length - 1, 17, arr));
+    System.out.println(floor(0, arr.length - 1, 17, arr));
   }
 
   static int solve(int s, int e, int t, int[] a){
@@ -20,5 +20,14 @@ class solve{
     if(a[mid] == t) return a[mid];
     else if(a[mid] > t) return ceil(s, mid - 1, t, a); 
     else return ceil(mid + 1, e, t, a);
+  }
+
+  // find floor of target if not present
+  static int floor(int s, int e, int t, int[] a){
+    if(s > e) return a[e];
+    int mid = s + (e - s) / 2;
+    if(a[mid] == t) return a[mid];
+    else if(a[mid] > t) return floor(s, mid - 1, t, a); 
+    else return floor(mid + 1, e, t, a);
   }
 }
